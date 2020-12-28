3398e4a73f86731f5b09f8f7756d6d447a9d76b9
Lee YuPyeong <stoneberg73@gmail.com>
2020-12-28 16:39:02 +0900

    minor update

--- src/main/java/kr/co/cesco/econtract/test/dto/PeopleDto.java
+++ src/main/java/kr/co/cesco/econtract/test/dto/PeopleDto.java
@@ -1,11 +1,13 @@
 package kr.co.cesco.econtract.test.dto;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 public class PeopleDto {
 	
 	@Data
-	public static class PersonDto {
+	public static class PersonDto implements Serializable {
 		private static final long serialVersionUID = 1L;
 		private Integer id;
 		private String name;
